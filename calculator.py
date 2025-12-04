5e7ab6e3664bde42cc0cc1c439fcc0ed6ac83a00
Gabriel Pedro <g.pedro0972@gmail.com>
2025-12-04 08:48:39 -0300

    Projeto Calcladora Basica

---
+++ calculator.py
@@ -0,0 +1,24 @@
+import math
+
+
+class Calculator:  
+    
+    def  __init__(self):
+        self.result = 0
+    
+    def add(self, x, y): # Soma 
+        return x + y
+    def substract(self, x, y): # Subtração
+        return x - y 
+    def multiply(self, x, y): # Multiplicação
+        return x * y
+    def division(self, x, y): # Divisão
+        if y == 0:
+            raise ValueError("Erro")
+        return x / y
+    def square_root(self, x): # Raiz Quadrada
+        return math.sqrt(x)
+    def power(self, x, y):
+        return math.pow(x, y)
+
+
